9932fe2ad1c35dd13569b7c4e92366b9e5a83a72
Kevin Mui <kmui.work@gmail.com>
2018-10-13 13:36:36 -0700

    Styling form.

--- src/client/js/components/Form.jsx
+++ src/client/js/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 
 class Form extends React.Component {
@@ -7,14 +7,14 @@ class Form extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.fileInput = React.createRef();
-
   }
+
   handleSubmit(event) {
     event.preventDefault();
     alert(
       `Selected file - ${
-      this.fileInput.current.files[0].name
-      }`
+        this.fileInput.current.files[0].name
+      }`,
     );
   }
   // where should these functions go
@@ -22,67 +22,68 @@ class Form extends React.Component {
   render() {
     return (
       <div className="formBackground">
-        <form className="uglyForm" onSubmit={this.handleSubmit}>
+        <div id="formContainer">
+          <form className="uglyForm" onSubmit={this.handleSubmit}>
+
+            <div>
+
+              <input className="formInput" id="formTitle" type="text" placeholder="Title" onChange={(event) => { this.props.updateTitle(event); }} />
+              <br />
+              <input className="formInput" id="formPrice" type="text" placeholder="Price" onChange={(event) => { this.props.updatePrice(event); }} />
+              <br />
+
+
+              <select className="formInput" id="selectBrand" onChange={(event) => { this.props.updateSelectedBrand(event); }}>
+                <option defaultValue="test"> -- Brand -- </option>
+                <option>Nike</option>
+                <option>adidas</option>
+                <option>Jordan</option>
+                <option>Converse</option>
+
+              </select>
+              <br />
+
+              <select className="formInput" id="selectSize" onChange={(event) => { this.props.updateSelectedSize(event); }}>
+                <option defaultValue="test"> -- Size -- </option>
+                <option>7</option>
+                <option>7.5</option>
+                <option>8</option>
+                <option>8.5</option>
+                <option>9</option>
+                <option>9.5</option>
+                <option>10</option>
+                <option>10.5</option>
+                <option>11</option>
+                <option>11.5</option>
+                <option>12</option>
+              </select>
+              <br />
+
+              <select className="formInput" id="selectCondition" onChange={(event) => { this.props.updateSelectedCondition(event); }}>
+                <option defaultValue="test"> -- Conditions -- </option>
+                <option>Mint Deadstock</option>
+                <option>Deadstock</option>
+                <option>Near Deadstock</option>
+                <option>Excellent</option>
+                <option>Good</option>
+              </select>
+              <br />
+
+            </div>
+            <input className="formInput" id="imageInput" type="file" name="image" onChange={this.props.handleUploadImage} />
+            <br />
+            <button className="formInput" id="submitInput" type="submit" onClick={this.props.createNew}>Submit</button>
+            <button className="formInput" id="closeInput" onClick={this.props.togglePopup}>Close</button>
+          </form>
 
           <div>
-
-            <input type="text" placeholder="title" onChange={(event) => { this.props.updateTitle(event); }}></input><br></br>
-            <input type="text" placeholder="price" onChange={(event) => { this.props.updatePrice(event); }}></input><br></br>
-          
-
-            <select id="selectBrand" onChange={(event) => { this.props.updateSelectedBrand(event); }}>
-              <option defaultValue="test"> -- Brand -- </option>
-               <option>Nike</option>
-               <option>adidas</option>
-               <option>Jordan</option>
-               <option>Converse</option>
-
-            </select>
-            <br></br>
-
-            <select id="selectSize" onChange={(event) => { this.props.updateSelectedSize(event); }}>
-              <option defaultValue="test"> -- Size -- </option>
-              <option>7</option>
-              <option>7.5</option>
-              <option>8</option>
-              <option>8.5</option>
-              <option>9</option>
-              <option>9.5</option>
-              <option>10</option>
-              <option>10.5</option>
-              <option>11</option>
-              <option>11.5</option>
-              <option>12</option>
-            </select>
-            <br></br>
-
-            <select id="selectCondition" onChange={(event) => { this.props.updateSelectedCondition(event); }}>
-              <option defaultValue="test"> -- Conditions -- </option>
-              <option>Mint Deadstock</option>
-              <option>Deadstock</option>
-              <option>Near Deadstock</option>
-              <option>Excellent</option>
-              <option>Good</option>
-            </select>
-            <br></br>
-
-          </div>
-          <button type="submit" onClick={this.props.createNew} >Submit</button>
-          <button onClick={this.props.togglePopup}>close me</button>
-        </form>
-
-          <div>
-            <form  /*onSubmit={(event) => {
-              event.preventDefault();
-              console.log('event reached');
-              console.log(event.target);
-              this.props.handleUploadImage(event)}}*/>
-              <input type='file' name="image" onChange={this.props.handleUploadImage}/><br/>
-              <input type="submit" value="Upload" />
+            <form>
+              {/* <input type='file' name="image" onChange={this.props.handleUploadImage}/><br/>
+              <input type="submit" value="Upload" /> */}
               {/* <button onClick={} >Upload</button> */}
             </form>
           </div>
-
+        </div>
       </div>
     );
   }
