3a5d2348b61b8842b75130bc16f9fb447d905fdc
Kevin Mui <kmui.work@gmail.com>
2018-10-13 10:10:58 -0700

    Complete filter functionality.

---
+++ src/client/js/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Navigation = () => (
+  <div id="navBar" />
+);
+
+export default Navigation;
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const userController = require('./controllers/userController.js');
 const parser = require('./cloudinary.js');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/signup', userController.createUser);
 app.post('/login', userController.getUser);
@@ -14,10 +15,13 @@ app.post('/listing', userController.postListing);
 app.get('/filterbybrand/:brand', userController.filterByBrand);
 app.get('/filterbycondition/:condition', userController.filterByCondition);
 app.get('/filterbyuser/:uid', userController.filterByUser);
-app.post('/images', parser.single("image"), userController.imageParser);
-app.use(express.static(__dirname + '/../../dist'));
+app.get('/filterbysize/:size', userController.filterBySize);
+app.post('/images', parser.single('image'), userController.imageParser);
+app.get('/categories/:filter', userController.getCategories);
+app.use(express.static(`${__dirname }/../../dist`));
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
   else console.log('Listening on port 3000...');
-})
+});
+
